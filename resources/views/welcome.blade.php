0dc94a0c89643b524926925e1d245f474791a4ca
stu p <stu_3881@yahoo.com>
2016-10-22 13:07:23 -0700

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>l535i_master</title>
+        <title>l5_larablues2</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel l535i master
+                    Laravel l5_larablues2
                 </div>
 
                 <div class="links">
