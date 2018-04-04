66454c602c8b95c60169d93761749e863296b7ba
stu p <stu_3881@yahoo.com>
2018-04-04 08:02:00 -0700

    xx

---
+++ resources/views/miscThings/generated_files/12667_browse_select_display_snippet.blade.php
@@ -0,0 +1,12 @@
+
+<td class='text_align_left select_pink' >
+{{ Form::label('', $record->business_rules_field_name_array) }}
+</td>
+
+<td class='text_align_left select_pink' >
+{{ Form::label('', $record->business_rules_r_o_array) }}
+</td>
+
+<td class='text_align_left select_pink' >
+{{ Form::label('', $record->business_rules_value_array) }}
+</td>
---
+++ resources/views/miscThings/generated_files/12667_browse_select_field_names_row.blade.php
@@ -0,0 +1,6 @@
+<tr>
+<td>#</td>
+<td>business_rules_field_name_array</td>
+<td>business_rules_r_o_array</td>
+<td>business_rules_value_array</td>
+</tr>
